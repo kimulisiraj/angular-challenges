f887dec34083254cf76152ef8fdba1fe85135e82
Siraj Kimuli <pworld.designer@gmail.com>
2024-09-02 07:56:39 +0300

    feat: answer to anchor-navigation
    adds anchor

--- apps/angular/21-anchor-navigation/src/app/foo.component.ts
+++ apps/angular/21-anchor-navigation/src/app/foo.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NavButtonComponent } from './nav-button.component';
+import { ScrollService } from './scroll.service';
 
 @Component({
   standalone: true,
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, RouterModule],
   selector: 'app-foo',
   template: `
     Welcome to foo page
-    <nav-button href="home" class="fixed left-1/2 top-3">Home Page</nav-button>
-    <div class="h-screen bg-blue-200">section 1</div>
-    <div class="h-screen bg-red-200">section 2</div>
+    <app-nav-button [routerLink]="'home'" class="fixed left-1/2 top-3">
+      Home Page
+    </app-nav-button>
+    <div
+      id="section1"
+      class="h-screen bg-blue-200"
+      (click)="this.scroll.scrollTo('section2')">
+      section 1
+      <button class="m-2 block w-fit rounded-md border border-red-500 p-4">
+        Section 2
+      </button>
+    </div>
+    <div
+      id="section2"
+      class="h-screen bg-red-200"
+      (click)="this.scroll.scrollTo('section1')">
+      section 2
+      <button class="m-2 block w-fit rounded-md border border-red-500 p-4">
+        Section 1
+      </button>
+    </div>
   `,
 })
-export class FooComponent {}
+export class FooComponent {
+  scroll = inject(ScrollService);
+}
--- apps/angular/21-anchor-navigation/src/app/home.component.ts
+++ apps/angular/21-anchor-navigation/src/app/home.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NavButtonComponent } from './nav-button.component';
+import { ScrollService } from './scroll.service';
 
 @Component({
   standalone: true,
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, RouterModule],
   selector: 'app-home',
   template: `
-    <nav-button href="/foo" class="fixed left-1/2 top-3">Foo Page</nav-button>
-    <div id="top" class="h-screen bg-gray-500">
+    <app-nav-button [routerLink]="'/foo'" class="fixed left-1/2 top-3">
+      Foo Page
+    </app-nav-button>
+    <div
+      id="top"
+      class="h-screen bg-gray-500"
+      (click)="this.scroll.scrollTo('bottom')">
       Empty
-      <nav-button href="#bottom">Scroll Bottom</nav-button>
+      <app-nav-button>Scroll Bottom</app-nav-button>
     </div>
-    <div id="bottom" class="h-screen bg-blue-300">
+    <div
+      id="bottom"
+      class="h-screen bg-blue-300"
+      (click)="this.scroll.scrollTo('top')">
       I want to scroll each
-      <nav-button href="#top">Scroll Top</nav-button>
+      <app-nav-button>Scroll Top</app-nav-button>
     </div>
   `,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  scroll = inject(ScrollService);
+}
